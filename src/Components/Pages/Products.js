7559da6c0fe1810509426313d3463ef7d62c88f5
B. M. Mahbubul Alam <108087360+phmahbub@users.noreply.github.com>
2022-11-01 23:00:15 +0600

    CR

--- src/Components/Pages/Products.js
+++ src/Components/Pages/Products.js
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 const Products = () => {
-  const handleSubmit = (event) => {};
+
+  const [user, setUser] = useState({})
+
+  const handleOnchange = (event) =>{
+    const value = event.target.value
+    const field = event.target.name
+    const newUser = {...user}
+    newUser[field] = value
+    setUser(newUser)
+  }
+
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(user)
+//process of sending data from client to server------ fetch , method(post/put), headers, body
+
+    fetch('http://localhost:5000/student', {
+      method: 'POST',
+      headers: {
+        'content-type' : 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=>res.json())
+    .then(data => {
+      if(data.acknowledged){
+        alert("Data added successfully")
+        event.target.reset()
+      }
+     
+      
+      console.log(data)
+    
+    })
+
+  }
+
   return (
     <div>
     <Form onSubmit={handleSubmit}>
@@ -10,25 +48,31 @@ const Products = () => {
       <Form.Group className="my-3" controlId="product">
         <Form.Label>Product Name</Form.Label>
         <Form.Control
+        onChange={handleOnchange}
           name="product"
           type="product"
           placeholder="Enter Your Product Name"
+          required
         />
       </Form.Group>
       <Form.Group className="mb-3" controlId="product">
-        <Form.Label>Product Name</Form.Label>
+        <Form.Label>Product Price</Form.Label>
         <Form.Control
+          onChange={handleOnchange}
           name="price"
-          type="price"
+          type="number"
           placeholder="Enter Your price Name"
+          required
         />
       </Form.Group>
       <Form.Group className="mb-3" controlId="product">
-        <Form.Label>Product Name</Form.Label>
+        <Form.Label>Product Quantity</Form.Label>
         <Form.Control
+        onChange={handleOnchange}
           name="quantity"
-          type="quantity"
+          type="number"
           placeholder="Enter Product Quantity"
+          required
         />
       </Form.Group>
 
